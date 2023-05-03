f0263def9411783cf154bc9bb0542e7f28bd78c2
rahulshukladraftbit <115546443+rahulshukladraftbit@users.noreply.github.com>
2023-05-03 21:21:49 +0530

    test

--- screens/BlankScreen.js
+++ screens/BlankScreen.js
@@ -22,7 +22,12 @@ const BlankScreen = props => {
     const { coords } = await CustomCode.Location.getCurrentPositionAsync({});
     return coords;
   };
+  // Type the code for the body of your function or hook here.
+  // Functions can be triggered via Button/Touchable actions.
+  // Hooks are run per ReactJS rules.
 
+  /* String line breaks are accomplished with backticks ( example: `line one
+line two` ) and will not work with special characters inside of quotes ( example: "line one line two" ) */
   const { theme } = props;
 
   const [error, setError] = React.useState('');
